4ab5187cd69c43c389e1a3d0287774301d4e139c
Nguyễn Minh Thuận <nmthuan.nmt@gmail.com>
2021-08-27 19:20:32 +0700

    ???

--- _tmp_thangDong1.py
+++ _tmp_thangDong1.py
@@ -1,9 +1,7 @@
 import cv2
 import numpy as np
-from scipy.interpolate import CubicSpline
+from scipy.interpolate import CubicSpline, UnivariateSpline
 import numpy as np
-import matplotlib.pyplot as plt
-
 
 def findPointList(contours):
     res = []
@@ -15,57 +13,91 @@ def findPointList(contours):
 
     return res
 
-def getLineMask(point_lists, shape):
-    mask = np.zeros(shape) + 255
+def get_next_contour(mask, contour):
+    maxY, maxX = mask.shape
 
-    pts = np.array(point_lists, dtype=np.int32)
-    cv2.polylines(mask, [pts], False, (123,123,123), thickness=1, lineType=cv2.LINE_AA)
+    M = cv2.moments(contour)
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
 
-    return mask
+    current_contour = mask[cY][cX]
+    while cX < maxX:
+        if mask[cY][cX] != current_contour and mask[cY][cX] != 0:
+            return current_contour, mask[cY][cX]
+        cX += 1
+
+    return current_contour, None
+
+def connect_pair(pairs):
+    for pair in pairs:
+        return
 
 img = cv2.imread('demo7.png')
 gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 bin = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 101, 20)
 
 revert = 255 - bin
-# kernel = np.ones((1, 20), dtype=np.uint8)
-# dilate = cv2.dilate(revert, kernel)
-
 dilate = cv2.dilate(revert, None, iterations=2)
 
 contours, _ = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+contours.sort(key=lambda ctr: cv2.boundingRect(ctr)[0] + cv2.boundingRect(ctr)[1] * img.shape[1])
+
+check = [0] * len(contours)
+lines = []
+mask = np.zeros(gray.shape)
+for i, contour in enumerate(contours):
+    cv2.fillPoly(mask, pts =[contour], color= i + 1)
+
+pair = []
+for contour in contours:
+    pair.append(get_next_contour(mask, contour))
+
+print(pair)
+lines = connect_pair(pair)
+
+cv2.imwrite('abczyx.png', mask)
+exit()
+
+
+
+
+
+
 
 new = np.zeros(img.shape) + 255
 
 cv2.drawContours(img, contours, -1, (0, 255, 0), 1)
 points = findPointList(contours)
-points.sort()
-
-points = zip(*points)
-points = list(points)
+points.sort()   
 
 
-f = CubicSpline(points[0], points[1], bc_type='natural')
-x_new = np.linspace(0, img.shape[1], img.shape[1] + 1)
-y_new = f(x_new)
+# #convert point list
+# _points = zip(*points)
+# _points = list(_points)
+
+# f = UnivariateSpline(_points[0], _points[1])
+# # f = CubicSpline(_points[0], _points[1])
+# x_new = np.linspace(0, img.shape[1], img.shape[1] + 1)
+# y_new = f(x_new)
 
 # for point in points:
 #     cv2.circle(img, point, radius=3, color=(0, 0, 255), thickness = -1)
 
-mask = np.zeros(gray.shape)
+# mask = np.zeros(gray.shape)
 # pts = np.array(points, dtype=np.int32)
-# cv2.polylines(mask, [pts], False, 255, thickness=1, lineType=cv2.LINE_AA)
+# cv2.polylines(img, [pts], False, 255, thickness=1, lineType=cv2.LINE_AA)
 
-mainAxis = mask.shape[0] / 2
-for x in range(0, mask.shape[1]):
-    delta = mainAxis - y_new[x]
+# mainAxis = mask.shape[0] / 2
+# for x in range(0, mask.shape[1]):
+#     delta = mainAxis - y_new[x]
 
-    for y in range(0, gray.shape[0]):
-        if bin[y][x] == 0:
-            new[int(y + delta)][x] = 0
+#     for y in range(0, gray.shape[0]):
+#         if bin[y][x] == 0:
+#             new[y][x] = (200, 200, 200)
+#             new[int(y + delta)][x] = 0
 
-# for y in range(0, mask.shape[1]):
-#     maskPoint = mainAxis
+# # for y in range(0, mask.shape[1]):
+# #     maskPoint = mainAxis
 #     for x in range(0, mask.shape[0]):
 #         if mask[x][y] == 255:
 #             maskPoint = x
@@ -77,4 +109,4 @@ for x in range(0, mask.shape[1]):
 #         if bin[x][y] == 0:
 #             new[int(x + delta)][y] = 0
 
-cv2.imwrite('abczyx.png', new)
+cv2.imwrite('abczyx.png', mask)
